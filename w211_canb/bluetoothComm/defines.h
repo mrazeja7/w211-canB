af3184ab40f049111008c3fdb1786adfb3e297d4
Jan Mrazek <mrazeja7@ksu.edu>
2020-02-22 16:01:31 +0100

    flip BT pins so they make sense

--- w211_canb/bluetoothComm/defines.h
+++ w211_canb/bluetoothComm/defines.h
@@ -1,10 +1,10 @@
 // enables 'verbose mode' of the BT module
 #define DEBUG 1
 
-#define BLUETOOTH_RX D2
-#define BLUETOOTH_TX D1
+#define BLUETOOTH_RX D1
+#define BLUETOOTH_TX D2
 
-// the duration of the media keypresses - after this time is elapsed, the module sends a HID report where no keys are pressed (thus simulating a key release)
+// the duration of the media keypresses (in ms) - after this time is elapsed, the module sends a HID report where no keys are pressed (thus simulating a key release)
 #define BLUETOOTH_KEY_DELAY 75
 
 // Raw HID report data for all the buttons this module uses
