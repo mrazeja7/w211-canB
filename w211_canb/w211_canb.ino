82a01a9966e92028afbe04f0ceae80cbba54fd7f
Jan Mrazek <mrazeja7@gmail.com>
2020-11-07 21:04:28 +0100

    clean up device setup code; add crude version of button parsing

--- w211_canb/w211_canb.ino
+++ w211_canb/w211_canb.ino
@@ -1,10 +1,10 @@
+// MCP2515 library verbose mode
+#define DEBUG_MODE 1
+
 #include "mcp_can.h"
 #include <SPI.h>
 
-// CAN TX Variables
-unsigned long prevTX = 0;                                        // Variable to store last execution time
-const unsigned int invlTX = 1000;                                // One second interval constant
-byte data[] = {0xAA, 0x55, 0x01, 0x10, 0xFF, 0x12, 0x34, 0x56};  // Generic CAN data to send
+byte data[] = {0xAA, 0x55, 0x01, 0x10, 0xFF, 0x12, 0x34, 0x56};  // CAN test message
 
 // CAN RX Variables
 long unsigned int rxId;
@@ -24,29 +24,97 @@ MCP_CAN CAN0(15); // NodeMCU D8
 void setup()
 {
   Serial.begin(115200);
+  Serial.println("10 second grace period starting; start programming firmware now!");
+  delay(10000);
+  Serial.println("Grace period over.");
   
-    if (CAN0.begin(MCP_ANY, CAN_83K33BPS, MCP_16MHZ) == CAN_OK) // my own values
-  if (CAN0.begin(MCP_ANY, CAN_83K33BPS_AA, MCP_16MHZ) == CAN_OK) // angelovAlex's suggested values from this MBWorld post: https://mbworld.org/forums/audio-electronics/580108-w211-can-b-hacking-2.html#post7038959
-    Serial.println("MCP2515 Initialized Successfully!");
-  else
-    Serial.println("Error Initializing MCP2515...");
-  
-  // Since we do not set NORMAL mode, we are in loopback mode by default.
-  CAN0.setMode(MCP_NORMAL);
+  int canStatus = CAN_FAIL;
 
-  pinMode(CAN0_INT, INPUT); // Configuring pin for /INT input
+  // my own values
+  // if ( (canStatus = CAN0.begin(MCP_ANY, CAN_83K33BPS, MCP_16MHZ)) != CAN_OK)
+
+  // angelovAlex's suggested values from this MBWorld post: https://mbworld.org/forums/audio-electronics/580108-w211-can-b-hacking-2.html#post7038959
+  if ( (canStatus = CAN0.begin(MCP_ANY, CAN_83K33BPS_AA, MCP_16MHZ)) != CAN_OK)
+  {
+    Serial.print("MCP2515 init error: ");
+    Serial.println(canStatus, HEX);
+  } 
+   
+  if ( (canStatus = CAN0.setMode(MCP_NORMAL)) != CAN_OK)
+  {
+    Serial.print("MCP2515 setMode error: ");
+    Serial.println(canStatus, HEX);
+  }
+    
+  Serial.println("MCP2515 init ok");
+
+  pinMode(CAN0_INT, INPUT); // configure pin for interrupt input
+}
+
+unsigned long nonUseful = 0;
+int lastPressed = 0;
+
+// return value indicates whether a command should be sent over bluetooth; if so, it specifies which one
+int parseButtons(unsigned char *rxBuf, unsigned char len)
+{
+  switch (rxBuf[1])
+  {
+    // arrow buttons
+    case 0x01:
+      Serial.print(" - up arrow pressed");
+      lastPressed = rxBuf[0] << 1 | rxBuf[1];
+      break;
+    case 0x02:
+      Serial.print(" - down arrow pressed");
+      lastPressed = rxBuf[0] << 1 | rxBuf[1];
+      break;
+    case 0x03:
+      Serial.print(" - up+down arrows pressed");
+      lastPressed = rxBuf[0] << 1 | rxBuf[1];
+      break;
+
+    // volume buttons
+    case 0x10:
+      Serial.print(" - \"+\" button pressed");
+      lastPressed = rxBuf[0] << 1 | rxBuf[1];
+      break;
+    case 0x20:
+      Serial.print(" - \"-\" button pressed");
+      lastPressed = rxBuf[0] << 1 | rxBuf[1];
+      break;
+    case 0x30:
+      Serial.print(" - \"+\"+\"-\" buttons pressed");
+      lastPressed = rxBuf[0] << 1 | rxBuf[1];
+      break;
+      
+    case 0x00: // buttons released
+      Serial.print(" - buttons released - the last pressed button was ");
+      Serial.print(lastPressed, HEX);
+
+      // if the lastPressed variable equals zero here, a debouncing issue occurred -> don't send a command
+      if (lastPressed)
+      {
+        int tmp = lastPressed;
+        lastPressed = 0;
+        return tmp;
+      }
+  }
+  return 0;
 }
 
 void loop()
-{
+{  
   if(!digitalRead(CAN0_INT))
   {
     memset(rxBuf, 0, 8);
     int msgTime = millis();
     CAN0.readMsgBuf(&rxId, &len, rxBuf);
 
-    //if (rxId != 0x2C && rxId != 0x1CA) // filter out everything except lower center panel and steering wheel buttons
-    //  return;
+    if (rxId != 0x2C && rxId != 0x1CA) // filter out everything except lower center panel and steering wheel buttons
+    {
+      nonUseful++;
+      return;
+    }
     
     if((rxId & 0x80000000) == 0x80000000) // Determine if ID is standard (11 bits) or extended (29 bits)
       sprintf(msgString, "%d:\tEID: 0x%.8lX\tlen: %1d\tData:", msgTime, (rxId & 0x1FFFFFFF), len);
@@ -54,22 +122,40 @@ void loop()
       sprintf(msgString, "%d:\tSID: 0x%.3lX\tlen: %1d\tData:", msgTime, rxId, len);
   
     Serial.print(msgString);
-  
-    if((rxId & 0x40000000) == 0x40000000) // remote request frame - will never be used in this project
+
+    /* remote request frame - will never be used in this project    
+    if((rxId & 0x40000000) == 0x40000000) 
     {
       sprintf(msgString, " REMOTE REQUEST FRAME");
       Serial.print(msgString);
     } 
-    else 
+    */
+    
+    for(byte i = 0; i<len; i++)
     {
-      for(byte i = 0; i<len; i++)
-      {
-        sprintf(msgString, " 0x%.2X", rxBuf[i]);
-        Serial.print(msgString);
-      }
+      sprintf(msgString, " 0x%.2X", rxBuf[i]);
+      Serial.print(msgString);
+    }
+
+    // parse steering wheel buttons
+    if (rxBuf[0] == 0x03)
+    {
+        int msg = parseButtons(rxBuf, len);
+        if (msg)
+        {
+          // handle bluetooth communication here
+        }
     }
         
     Serial.println();
+  }
+
+  if (millis() % 10000 == 0)
+  {
+    Serial.print(nonUseful);
+    Serial.println(" non-useful messages in the past 10 seconds");
+    nonUseful = 0;
+    delayMicroseconds(980);
   }
 
 /*
@@ -88,7 +174,3 @@ void loop()
   }
 */
 }
-
-/*********************************************************************************************************
-  END FILE
-*********************************************************************************************************/
